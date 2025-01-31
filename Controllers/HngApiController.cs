32dcd4f3176c9a24224ecba5164c5f8e2fb88a2f
DAUDU QAZIM <yetkemsupper@gmail.com>
2025-01-31 20:32:56 +0100

    InitialCreate

---
+++ Controllers/HngApiController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HngAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HngApiController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetDetails()
+        {
+            return Ok(new
+            {
+                email = "yetkemsupper@gmail.com", // Replace with your HNG email
+                current_datetime = DateTime.UtcNow.ToString("o"), // ISO 8601 format
+                github_url = "https://github.com/Qazim-tec/HNGPublicAPI.git" // Replace with your GitHub repo
+            });
+        }
+    }
+}
